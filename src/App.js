da0bf169cb3ffd9bf3f47e39579c8af38d415c46
John Storey <jstorey@wayfair.com>
2020-02-25 22:52:54 -0500

    adds location

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import './Shorthand.css';
-import Vote from './routes/vote'
-import Landing from './routes/landing'
-import GlobalWrapper from './stores/global'
-import Toast from './components/Toast' 
-import 'semantic-ui-css/semantic.min.css' 
+import "./App.css";
+import "./Shorthand.css";
+import Vote from "./routes/vote";
+import Landing from "./routes/landing";
+import GlobalWrapper from "./stores/global";
+import Toast from "./components/Toast";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
-    return (
+  return (
     <GlobalWrapper>
       <Toast>
         <Router>
-          <Route path="/vote/:baseId" component={Vote} />
+          <Route path="/vote/:baseId/:location" component={Vote} />
           <Route exact path="/" component={Landing} />
           {/* <Route exact path="*" component={()=><div className='flex jcc mt20 fs3'>Page Not Found</div>} /> */}
         </Router>
